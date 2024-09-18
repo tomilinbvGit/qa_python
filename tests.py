78a38c66de577b3351bc0bc2e40f4c40201a9f65
tomilinBV <qwertere97@mail.ru>
2024-09-18 15:10:37 +0300

    qa_python_second_sprint_first_commit

--- tests.py
+++ tests.py
@@ -1,24 +1,123 @@
 from main import BooksCollector
+import pytest
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_book_added(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        # добавляем книгу
+        collector.add_new_book('Три стигмата Палмера Элдрича')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # проверяем, что книга добавлена
+        assert len(collector.books_genre) == 1
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_two_identical_books_one_is_added(self):  #пробуем добавить две одинаковые книги
+        collector = BooksCollector()
+
+        collector.add_new_book('Пролейтесь, слёзы...')
+        collector.add_new_book('Пролейтесь, слёзы...')
+
+        assert len(collector.books_genre) == 1  #проверяем, что добавлена только одна книга
+
+    def test_set_book_genre_genre_sets(self):  #тестируем установку жанра для книги
+        collector = BooksCollector()
+
+        collector.add_new_book('Мечтают ли андроиды об электроовцах?')
+        collector.set_book_genre('Мечтают ли андроиды об электроовцах?', 'Фантастика')
+
+        assert collector.books_genre[
+                   'Мечтают ли андроиды об электроовцах?'] == 'Фантастика'  #проверяем, что жанр установлен
+
+    def test_set_book_genre_set_genre_not_from_list(self):  #тестируем установку жанра не из списка
+        collector = BooksCollector()
+
+        collector.add_new_book('Сдвиг времени по-марсиански')
+        collector.set_book_genre('Сдвиг времени по-марсиански', 'Хроноопера')
+
+        assert collector.books_genre['Сдвиг времени по-марсиански'] is ''  #жанр не будет добавлен
+
+    def test_get_book_genre_genre_returned(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Свидание с Рамой')
+        collector.set_book_genre('Свидание с Рамой', 'Фантастика')
+
+        assert collector.books_genre['Свидание с Рамой'] == 'Фантастика'
+
+    def test_get_books_with_specific_genre_the_correct_list_is_displayed(self):  #проверяем, что выводятся книги только с указанным жанром
+        collector = BooksCollector()
+
+        collector.add_new_book('Франкенштейн, или Современный Прометей')
+        collector.set_book_genre('Франкенштейн, или Современный Прометей', 'Ужасы')
+
+        collector.add_new_book('Тень над Иннсмутом')
+        collector.set_book_genre('Тень над Иннсмутом', 'Ужасы')
+
+        collector.add_new_book('Дети Мафусаила')
+        collector.set_book_genre('Дети Мафусаила', 'Фантастика')
+
+        assert len(collector.get_books_with_specific_genre('Ужасы')) == 2
+
+    def test_get_books_genre_all_elements_are_displayed(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Лунный камень')
+        collector.add_new_book('Ошибка резидента')
+        collector.add_new_book('Незнайка на Луне')
+
+        assert len(collector.get_books_genre()) == 3
+
+    def test_get_books_for_children_returned_books_for_children(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Мечтают ли андроиды об электроовцах?')
+        collector.set_book_genre('Мечтают ли андроиды об электроовцах?', 'Фантастика')
+
+        collector.add_new_book('Тень над Иннсмутом')
+        collector.set_book_genre('Тень над Иннсмутом', 'Ужасы')
+
+        collector.add_new_book('Незнайка на Луне')
+        collector.set_book_genre('Незнайка на Луне', 'Комедии')
+
+        collector.add_new_book('Маленький принц')
+        collector.set_book_genre('Маленький принц', 'Мультфильмы')
+
+        children_books_len = len(collector.get_books_for_children())
+        assert children_books_len == 3
+
+    @pytest.mark.parametrize('name', ['Дюна', '451 градус по Фаренгейту', 'Задача трёх тел'])
+    def test_add_book_in_favorites_book_added(self, name):
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+
+        assert name in collector.favorites
+
+    @pytest.mark.parametrize('name', ['Дюна', '451 градус по Фаренгейту', 'Задача трёх тел'])
+    def test_delete_book_from_favorites_book_deleted(self, name):
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        collector.delete_book_from_favorites(name)
+
+        assert name not in collector.favorites
+
+    def test_get_list_of_favorites_books_list_returned(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Мечтают ли андроиды об электроовцах?')
+        collector.add_book_in_favorites('Мечтают ли андроиды об электроовцах?')
+
+        collector.add_new_book('Тень над Иннсмутом')
+        collector.add_book_in_favorites('Тень над Иннсмутом')
+
+        collector.add_new_book('Незнайка на Луне')
+        collector.add_book_in_favorites('Незнайка на Луне')
+
+        assert len(collector.favorites) == 3
